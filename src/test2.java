a1dec1e72262f1da44a82c0d19985b71caa02450
kulklex <77890825+kulklex@users.noreply.github.com>
2022-03-12 07:50:41 +0100

    Update test2.java

--- src/test2.java
+++ src/test2.java
@@ -4,11 +4,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.Scanner;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+
 
 public class test2 {
 
